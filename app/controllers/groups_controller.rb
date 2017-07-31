c2e83dcdab768d41d337ca0b1f30bc7c7dae7fbe
Daisuke-WAKAMATSU <daisuke@wakamatsudaisuke.local>
2017-07-31 11:23:54 +0900

    group_check_box

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,22 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_paramas)
+    if @group.save
+      redirect_to root_path, notice:"グループを作成しました。"
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  private
+  def group_paramas
+    params.require(:chat_group).permit(:name)
   end
 end
