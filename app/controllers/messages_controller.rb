ce405b7505673f39bec4d32c79df4efb94d15ae0
Daisuke-WAKAMATSU <daisuke@wakamatsudaisuke.local>
2017-08-01 18:30:35 +0900

    Edit message send

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,26 +1,27 @@
-  class MessagesController < ApplicationController
+class MessagesController < ApplicationController
 
-    def index
-      @message = Message.new
-      @groups = current_user.groups
-      @group = Group.find(params[:group_id])
-    end
-
-    def create
-      @message = Message.new(message_paramas)
-      if @message.save
-        redirect_to action: :index
-      else
-        render :index
-      end
-    end
-
-    def update
-    end
-
-    private
-    def message_paramas
-      params.require(:message).permit(:body).merge(group_id: params[:group_id],user_id: current_user.id)
-    end
-
+  def index
+    @message = Message.new
+    @groups = current_user.groups
+    @group = Group.find(params[:group_id])
   end
+
+  def create
+    @message = current_user.messages.new(message_paramas)
+    binding.pry
+    if @message.save
+      redirect_to action: :index
+    else
+      redirect_to action: :index
+    end
+  end
+
+  def update
+  end
+
+  private
+  def message_paramas
+    params.require(:message).permit(:body).merge(group_id: params[:group_id])
+  end
+
+end
