8c25bd4d47e39871d89d61e58183b4748054e4bb
Daisuke-WAKAMATSU <daisuke@wakamatsudaisuke.local>
2017-08-03 01:07:20 +0900

    edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
-
 end
 
