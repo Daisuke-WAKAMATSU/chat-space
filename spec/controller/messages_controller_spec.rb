562a288cf27a6e0cdc036f4a66245bd7ede9e078
Daisuke-WAKAMATSU <daisuke@wakamatsudaisuke.local>
2017-08-05 12:43:35 +0900

    Edit controller test

--- spec/controller/messages_controller_spec.rb
+++ spec/controller/messages_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
     let(:user) { create(:user) }
     let(:group) { create(:group) }
     let(:member) { create(:member) }
+    let(:message) { create(:message) }
 
   describe 'GET #index' do
     context "ログイン中" do
-
       before do
         login_user user
       end
@@ -21,6 +21,40 @@ require 'rails_helper'
         get :index, params: {group_id: group}
         expect(response).to render_template :index
       end
+
+      context "メッセージの保存に成功" do
+        before do
+          login_user user
+        end
+
+        it "メッセージの保存ができたか" do
+          message = build(:message)
+          expect(message).to be_valid
+        end
+
+        it "意図した画面に遷移しているか" do
+          message = create(:message)
+          post :create, params: { group_id: group, message: attributes_for(:message) }
+          expect(response).to redirect_to group_messages_path
+        end
+
+      end
+
+      context "メッセージの保存に失敗" do
+        before do
+          login_user user
+        end
+
+      #   it "メッセージの保存は行われなかったか" do
+      #     expect(response).to render_template :new
+      #   end
+
+      #   it "意図したビューが描画されているか" do
+      #     expect(response).to render_template :new
+      #   end
+
+      # end
+
     end
   end
 end
@@ -28,22 +62,12 @@ end
 
 
 #   #   #ログイン中で保存に成功(メッセージ作成)
-#   #     it "メッセージの保存ができたか" do
-#   #     expect(response).to render_template :new
-#   #     end
 
-#   #     it "意図した画面に遷移しているか" do
-#   #     expect(response).to render_template :new
-#   #     end
 
 #   #   #ログイン中で保存に失敗(メッセージ作成)
-#   #     it "メッセージの保存は行われなかったか" do
-#   #     expect(response).to render_template :new
-#   #     end
 
-#   #     it "意図したビューが描画されているか" do
-#   #     expect(response).to render_template :new
-#   #     end
+
+
 
 #   # #ログインしていない(表示)
 #   #   it "意図したビューにリダイレクトされているか" do
--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   factory :message do
     body                  "test"
     image                  Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/image/neko.jpg'))
-    user_id               "1"
-    group_id              "1"
+    user
+    group
   end
 end
