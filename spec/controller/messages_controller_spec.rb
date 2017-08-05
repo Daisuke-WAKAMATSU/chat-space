12e98cdcfaee8bdbe45e3e2182f9756af5beadf1
Daisuke-WAKAMATSU <daisuke@wakamatsudaisuke.local>
2017-08-05 15:40:34 +0900

    Edit controller test

--- spec/controller/messages_controller_spec.rb
+++ spec/controller/messages_controller_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
     let(:group) { create(:group) }
     let(:member) { create(:member) }
     let(:message) { create(:message) }
+    let(:index_params) { {group_id: group} }
+    let(:create_params) { { group_id: group, message: attributes_for(:message) } }
 
   describe 'GET #index' do
     context "ログインしている場合" do
@@ -12,20 +14,19 @@ require 'rails_helper'
       end
 
       it "アクション内で定義しているインスタンス変数があるか" do
-        group = create(:group)
-        get :index, params: {group_id: group}
+        get :index, params: index_params
         expect(assigns(:group)).to eq group
       end
 
       it "該当するビューが描画されているか" do
-        get :index, params: {group_id: group}
+        get :index, params: index_params
         expect(response).to render_template :index
       end
     end
 
     context "ログインしていない場合" do
       it "意図した画面にリダイレクトできているか" do
-        get :index, params: {group_id: group}
+        get :index, params: index_params
         expect(response).to redirect_to new_user_session_path
       end
     end
@@ -45,7 +46,7 @@ require 'rails_helper'
 
         it "意図した画面に遷移しているか" do
           message = create(:message)
-          post :create, params: { group_id: group, message: attributes_for(:message) }
+          post :create, params: create_params
           expect(response).to redirect_to group_messages_path
         end
       end
@@ -62,7 +63,7 @@ require 'rails_helper'
 
         it "意図したビューが描画されているか" do
           message = build(:message, body: "", image: "")
-          post :create, params: { group_id: group, message: attributes_for(:message) }
+          post :create, params: create_params
           expect(response).to redirect_to group_messages_path
         end
       end
@@ -70,7 +71,7 @@ require 'rails_helper'
 
     context "ログインしていない場合" do
       it "意図した画面にリダイレクトできているか" do
-          post :create, params: { group_id: group, message: attributes_for(:message) }
+          post :create, params: create_params
           expect(response).to redirect_to new_user_session_path
       end
     end
--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
 
   factory :message do
-    body                  "test"
-    image                  Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/image/neko.jpg'))
+    body                  { Faker::Name.name }
+    image                 { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/image/neko.jpg')) }
     user
     group
   end
