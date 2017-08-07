bd708e265909c86618575180b5c2715301e51b44
Daisuke-WAKAMATSU <daisuke@wakamatsudaisuke.local>
2017-08-07 18:15:27 +0900

    If edit
    条件分岐の修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,19 @@
 $(function() {
   var main_message = $(".main__message");
   function appendMessage(message) {
-    if (message.image) {
+    if (message.image && message.body) {
       var html = `<div class="main__message__send-name">${ message.user_name }</div>
                   <div class="main__message__send-time">${ message.time }</div>
                   <div class="main__message__content">${ message.body }</div>
                   <div class="main__message__content"><img src="${ message.image }"</div>`
-    } else  {
+    } else if (message.body){
       var html = `<div class="main__message__send-name">${ message.user_name }</div>
                   <div class="main__message__send-time">${ message.time }</div>
                   <div class="main__message__content">${ message.body }</div>`
+    } else {
+      var html = `<div class="main__message__send-name">${ message.user_name }</div>
+                  <div class="main__message__send-time">${ message.time }</div>
+                  <div class="main__message__content"><img src="${ message.image }"</div>`
     };
     main_message.append(html);
   }
@@ -30,12 +34,14 @@ $(function() {
     .done(function(message) {
       appendMessage(message)
       $("#message_body").val('');
+      $(".main__send__form-image").val('');
       $('.main__message').animate({
           'scrollTop': $('.main__message')[0].scrollHeight}, 'fast');
       $(".main__send-botton").prop('disabled', false);
     })
     .fail(function() {
       $("#message_body").val('');
+      $(".main__send__form-image").val('');
       $(".main__send-botton").prop('disabled', false);
       alert("メッセージ送信が失敗しました")
     })
