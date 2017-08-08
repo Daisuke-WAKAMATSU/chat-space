561b7cf5cd6f86a63bb70542d22f94034794944c
Daisuke-WAKAMATSU <daisuke@wakamatsudaisuke.local>
2017-08-08 17:27:37 +0900

    incremental_search
    .dataメソッドに変更、追記。
    .failの追記
    スペース追加
    ハッシュロケットからシンボルへ変更

--- app/assets/javascripts/channels/user.js
+++ app/assets/javascripts/channels/user.js
@@ -4,7 +4,7 @@ $(function() {
   function searchUsers(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.id } data-user-name=${ user.name }>追加</a>
                 </div>`
     user_search_result.append(html);
   }
@@ -12,14 +12,14 @@ $(function() {
   function searchMessage() {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">該当するユーザーはいません</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="user.id" data-user-name="user.name"></a>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="{ user.id }" data-user-name="${ user.name }"></a>
                 </div>`
     user_search_result.append(html);
   }
 
   function addUser(user_id, user_name) {
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                  <input name='group[user_ids][]' type='hidden' value=${user_id}>
+                  <input name='group[user_ids][]' type='hidden' value=${ user_id }>
                   <p class='chat-group-user__name'>${ user_name }</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`;
@@ -45,13 +45,16 @@ $(function() {
      }
      else {
       searchMessage();
-     }
-   })
+     };
+    })
+    .fail(function() {
+      alert('グループ登録に失敗しました');
+    })
   });
 
   $(document).on('click', '.chat-group-user__btn--add', function(){
-    var user_id = $(this).attr('data-user-id');
-    var user_name = $(this).attr('data-user-name');
+    var user_id = $(this).data('user-id');
+    var user_name = $(this).data('user-name');
     addUser(user_id, user_name);
     $(this).parent().remove();
   });
